afdf41fab356cd507dea95fc844e65ccc12b4ce9
LAB-USR-LNORTE <LAB-USR-LNORTE@grupoutp.net>
2025-05-22 11:40:04 -0500

    Actualizando el js y test.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-function add(a, b) {
-    return a + b;
+function add(...numbers) {
+    return numbers.reduce((acc, num) => acc + num, 0);
 }
 
 module.exports = { add };
--- src/index.test.js
+++ src/index.test.js
@@ -4,6 +4,18 @@ test('adds 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3);
 });
 
-test('adds 5 + 7 to equals 12', () => {
-    expect(add(5,7)).toBe(12);
+test('adds 5 + 7 to equal 12', () => {
+    expect(add(5, 7)).toBe(12);
+});
+
+test('adds multiple numbers 1 + 2 + 3 to equal 6', () => {
+    expect(add(1, 2, 3)).toBe(6);
+});
+
+test('adds zero to numbers should return the same number', () => {
+    expect(add(0, 4, 8)).toBe(12);
+});
+
+test('adds negative numbers correctly', () => {
+    expect(add(-3, -2, 5)).toBe(0);
 });
